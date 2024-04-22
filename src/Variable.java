2cf0dfe8cc306c64c30f167c98c1aad9acac1735
adriannbhp <adrianbimo43@gmail.com>
2024-04-22 22:03:22 +0700

    Hello World

---
+++ src/Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+
+    public static void main(String[] args) {
+        int x; //declaration
+
+        x = 123; //assignment
+
+        int y = 234; //initialization
+
+        System.out.println(x + y);
+    }
+}
